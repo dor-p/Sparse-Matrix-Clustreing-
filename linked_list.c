ea11c08506bec206c8fb325860606e79579048e5
computer <computer@computer-pc>
2020-06-16 19:47:58 +0300

    new generic linked list, we'll need this for more than one thing

---
+++ linked_list.c
@@ -0,0 +1,97 @@
+/*
+ * linked_list.c
+ *
+ *  Created on: 16 Jun 2020
+ *      Author: computer
+ */
+
+#include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+/*
+ * we assume that the argument lst is not NULL
+ */
+int add(linked_list *lst, void *val){
+
+	linked_list *new_next = (linked_list*)malloc(sizeof(linked_list));
+	new_next->value = malloc(sizeof(void));
+	new_next->next = (linked_list*)malloc(sizeof(linked_list));
+	if(new_next == NULL || new_next->value == NULL || new_next->next == NULL){
+		free(new_next);
+		free(new_next->value);
+		free(new_next->next);
+		return -1;
+	}
+
+	new_next->value = lst->value;
+	new_next->next = lst->next;
+	lst->next = new_next;
+	lst->value = val;
+
+	return 1;
+}
+
+/*
+ * note that there is more than one reason to get NULL:
+ * 1. *lst == NULL
+ * 2. (*lst)->value == NULL
+ */
+void* ecxtract_first(linked_list **lst){
+	void* res;
+	linked_list *tmp;
+	if((*lst) == NULL) return NULL;
+
+	res = (*lst)->value;
+	tmp = *lst;
+	*lst = (*lst)->next;
+	free(tmp->next);
+	free(tmp->value);
+	return res;
+}
+
+/*
+ * we assume that the argument lst is not NULL
+ */
+int for_each(linked_list *lst, int (*func)){
+	int as;
+	as = func(lst->value);
+	if(!as) return as;
+	if(lst->next != NULL){
+		return for_each(lst->next);
+	}
+	return as;
+}
+
+/*
+ * we assume that the argument lst is not NULL
+ */
+void free_LL(linked_list *lst){
+	free(lst->value);
+	if(lst->next != NULL) free_LL(lst->next);
+	free(lst);
+}
+
+
+/*
+ * Allocates a new linked-lists
+ */
+linked_list* allocate_list(void *val){
+	linked_list *res = (linked_list*)malloc(sizeof(linked_list));
+	res->value = malloc(sizeof(void));
+	res->next = (linked_list*)malloc(sizeof(linked_list));
+	if(res == NULL || res->value == NULL || res->next == NULL){
+		free(res);
+		free(res->value);
+		free(res->next);
+		return NULL;
+	}
+
+	res->add = add;
+	res->for_each = for_each;
+	res->free = free_LL;
+
+	return res;
+}
---
+++ linked_list.h
@@ -0,0 +1,48 @@
+/*
+ * linked_list.h
+ *
+ *  Created on: 16 Jun 2020
+ *      Author: computer
+ */
+
+#ifndef LINKED_LIST_H_
+#define LINKED_LIST_H_
+
+typedef struct _linked_list {
+	/*
+	 * the value of the current node
+	 */
+	void *value;
+	struct _linked_list *next;
+
+	/*
+	 * Add a new element at the start of the list
+	 * return value represents a successful add
+	 */
+	int (*add)(struct _linked_list *lst, void *val);
+
+	/*
+	 * removes the first element and returns its value
+	 */
+	void* (*ecxtract_first)(struct _linked_list *lst);
+
+	/*
+	 * Iterates over the list and and performs func on each value
+	 */
+	int (*for_each)(struct _linked_list *lst, int (*func));
+
+
+	/*
+	 * Free all nodes from this one to the end
+	 */
+	void	(*free)(struct _linked_list *lst);
+
+} linked_list;
+
+/*
+ * Allocates a new linked-lists
+ */
+linked_list* allocate_list(void *val);
+
+
+#endif /* LINKED_LIST_H_ */
