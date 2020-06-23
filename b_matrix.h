5bf83f480f24b66cb598153ed5ce456e835cb3f6
computer <computer@computer-pc>
2020-06-23 19:37:09 +0300

    header for implementation of B, and updates as a result of this

---
+++ b_matrix.h
@@ -0,0 +1,39 @@
+/*
+ * b_matrix.h
+ *
+ *  Created on: 23 Jun 2020
+ *      Author: computer
+ */
+
+#ifndef B_MATRIX_H_
+#define B_MATRIX_H_
+
+#include "spmat_lists.h"
+
+typedef struct _B_matrix {
+	/*number of rows and columns in the matrix*/
+	int n;
+
+	/*vector of degrees of each vertex in the graph: k_i = sum(row[i])*/
+	int *k;
+
+	/*M = sum(k)*/
+	int M;
+
+	/*actual ajaisency matrix of the graph*/
+	spmat_lists *A;
+
+	/*a value to be added to the diagonal of the matrix
+	 *this is used for matrix shifting
+	 */
+	double lambda;
+
+	/*free all resources of this B matrix*/
+	void (*free)(struct _B_matrix*);
+
+} B_matrix;
+
+/*allocates a new B matrix and initializes it*/
+B_matrix* allocate_B(int n, int *k, spmat_lists *A);
+
+#endif /* B_MATRIX_H_ */
--- norm_1.c
+++ norm_1.c
@@ -5,7 +5,7 @@
  *      Author: computer
  */
 
-#include "matrix_row.h"
+#include "b_matrix.h"
 #include "spmat_lists.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,34 +14,41 @@
 
 /*
  * this is the function for the for_each
- */
+ *
 int add_to_norm(matrix_element *elem, void *column_sums){
 	double *double_sums = (double*)column_sums;
 	double_sums[elem->column] += fabs(elem->value);
 	return 1;
-}
+}*/
 
 /*
- *
+ *calculates the norm_1 of B
  */
-double norm_1(spmat_lists* A){
-	int i, as;
-	double res;
-	double *column_sums = (double*)calloc(A->n, sizeof(double));
+double norm_1(B_matrix* B){
+	int i, j;
+	double res, tmp;
+	linked_list *curr;
+	double *column_sums = (double*)calloc(B->n, sizeof(double));
 	if(column_sums == NULL){
 		free(column_sums);
 		return -1.0;
 	}
 	/*note that at this point column_sums has all 0 because calloc makes it so*/
-	for(i = 0; i < A->n; i++){
-		as = (A->rows)[i]->for_each((A->rows)[i], add_to_norm, column_sums);
-		if(!as){
-			free(column_sums);
-			return -1.0;
+	for(i = 0; i < B->n; i++){
+		curr = B->A->rows[i];
+		/*this assumes that the matrix rows are sorted*/
+		for(j = 0; j < B->n; j++){
+			tmp = -((double)(B->k[i] * B->k[j]) / (double)B->M);
+			if(i == j) tmp += B->lambda;
+			if(curr != NULL && (int*)curr->value == j){
+				tmp += 1;
+				curr = curr->next;
+			}
+			column_sums[j] += fabs(tmp);
 		}
 	}
 	res = 0.0;
-	for(i = 0; i < A->n; i++){
+	for(i = 0; i < B->n; i++){
 		if(column_sums[i] > res) res = column_sums[i];
 	}
 
